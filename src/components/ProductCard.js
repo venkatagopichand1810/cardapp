c5394aaab9b60529728fa5833d4b6e31207b5ac9
venkatguvi <venkatagopichand9@gmail.com>
2025-03-17 21:17:24 +0530

    updated

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,7 @@
 function ProductCard({product}) {
     return (
         <div className="product-card">
-            <img src={product.image} />
+            <img src={product.image} alt = {product.name}/>
             <h2>{product.name}</h2>
             <p>{product.price}</p>
             <small>{product.description}</small>
